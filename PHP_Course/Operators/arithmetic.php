ab6060feef476d744e7327bca96dfa8f53a8a616
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-25 21:04:39 +0100

    increments

---
+++ PHP_Course/Operators/arithmetic.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    // Increments
+    $x = 15;
+    echo ++$x; // 16
+    echo "<br>-------------<br>";
+    echo $x; // 16
+    echo "<br>-------------<br>";
+
+    $y = 15;
+    echo $y++; // 15
+    echo "<br>-------------<br>";
+    echo $y; // 16
+    echo "<br>-------------<br>";
+
+?>
+</body>
+</html>
