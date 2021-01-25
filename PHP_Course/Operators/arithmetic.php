771e3229a3fad771fa96c64c252c4a63511ac626
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-25 21:28:12 +0100

    switch

--- PHP_Course/Operators/arithmetic.php
+++ PHP_Course/Operators/arithmetic.php
@@ -9,11 +9,24 @@
     // Control structures
     $age = 22;
     if ($age < 18) {
-        echo "Sorry, you are too young";
+        echo "Sorry, you are too young<br>";
     } elseif ($age > 60) {
-        echo "Sorry, you are too old";
+        echo "Sorry, you are too old<br>";
     } else {
-        echo "You are old enogh to go out!";
+        echo "You are old enogh to go out!<br>";
+    }
+    // switch statement
+    $color = "red";
+    switch ($color) {
+        case 'green':
+            echo "You better paint it green<br>";
+            break;
+        case 'blue':
+            echo "You better paint it blue<br>";
+            break;
+        default:
+            echo "Do not paint it at all<br>";
+            break;
     }
     
 ?>
