3649459ca46632bb236f3b9cdee81cf003c9f610
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-26 14:00:00 +0100

    indlude

---
+++ PHP_Course/functions/about.php
@@ -0,0 +1,8 @@
+<?php
+    include 'includes/head.php';
+    include 'includes/header.php';
+?>
+<h1>About</h1>
+<?php
+    include 'includes/footer.php';
+?>
---
+++ PHP_Course/functions/index.php
@@ -0,0 +1,8 @@
+<?php
+    include 'includes/head.php';
+    include 'includes/header.php';
+?>
+<h1><?php introduction(); ?></h1>
+<?php
+    include 'includes/footer.php';
+?>
