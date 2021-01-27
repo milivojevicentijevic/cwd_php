1128f3daa09b4d64a29022ba4a29b9defadfcbd1
= <=>
2021-01-27 10:36:33 +0100

    get

---
+++ PHP_Course/superglobals/get.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get</title>
+</head>
+<body>
+<?php
+    // data will be visible in the URL
+
+?>
+ <form action="get.php" method="GET">
+    <input type="text" name="name">
+    <input type="password" name="password">
+    <button type="submit">SUBMIT</button>
+ </form>   
+</body>
+</html>
