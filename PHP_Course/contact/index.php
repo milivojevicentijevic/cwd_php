b9018abeba40ffadd63080e02ccce05d917cd769
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-27 16:26:03 +0100

    contact

---
+++ PHP_Course/contact/index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+    
+?> 
+<div>
+    <h1>Get in touch</h1>
+    <p>Please fill in the fields</p>
+    <form action="contact.php" method="post">
+        <input type="text" name="name" placeholder="Full name">
+        <input type="text" name="email" placeholder="E-mail">
+        <input type="text" name="subject" placeholder="Subject">
+        <textarea name="message" placeholder="Enter message"></textarea>
+        <button type="submit" name="submit">SEND E-MAIL</button>
+    </form>
+</div>
+</body>
+</html>
