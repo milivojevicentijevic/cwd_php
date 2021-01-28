494ee71759568459b1179446e93c872eed46e97a
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-28 15:05:33 +0100

    errors

--- PHP_Course/project/includes/register-inc.php
+++ PHP_Course/project/includes/register-inc.php
@@ -7,6 +7,45 @@ if(isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $confirmPass = $_POST['confirmPassword'];
+
+    if(empty($username) || empty($password) || empty($confirmPass)) {
+        header("Location: ../register.php?error=emptyfields&username=".$username);
+        exit();
+    } elseif(!preg_match("/^[a-zA-Z0-9]*/",$username)) { 
+        header("Location: ../register.php?error=invalidusername&username=".$username);
+        exit();
+    } elseif($password !== $confirmPass) {
+        header("Location: ../register.php?error=passwordsdonotmatch&username=".$username);
+        exit();
+    } else {
+        $sql = "SELECT username FROM users WHERE usrename = ?";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../register.php?error=sqlerror");
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $rowCount = mysqli_stmt_num_rows($stmt);
+
+            if($rowCount > 0) {
+                header("Location: ../register.php?error=usernametaken");
+                exit();
+            } else {
+                $sql = "INSERT INTO users(username, password) VALUES (?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+                if(!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../register.php?error=sqlerror");
+                    exit();
+                } else {
+                    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_store_result($stmt);
+                }
+            }
+        }
+    }
 }
 
 ?>
