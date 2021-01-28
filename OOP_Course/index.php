25d9d325ae366ee1d1d8bacc927cd019c6522a43
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-28 22:53:10 +0100

    final

--- OOP_Course/index.php
+++ OOP_Course/index.php
@@ -2,34 +2,16 @@
 
 class User 
 {
-    // properties
-    private $name;
-    private $age;
-
-    // constructor
-    public function __construct($name, $age)
-    {
-        $this->name = $name;
-        $this->age = $age;
-    }
-    public function __toString()
-    {
-        return "Name: ".$this->name."<br> Age: ".$this->age."<br>";
-    }
-    // getters and setters for name and age
-    public function setName($name) {
-        $this->name = $name;
-    }
-    public function getName() {
-        return $this->name."<br>";
-    }
-    public function setAge($age) {
-        $this->age = $age;
-    }
-    public function getAge() {
-        return $this->age."<br>";
+    public $name = "Jovan";
+    public $age;
+    final public function printName($name) {
+        return "My name is ".$name."<br>";
     }
 }
 
-$user = new User("Jovan", 46);
-echo $user;
+class Manager extends User {
+    public $name = "David";
+}
+
+$manager = new Manager();
+echo $manager->printName("Marko");
