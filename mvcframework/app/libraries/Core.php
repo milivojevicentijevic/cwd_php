49654c0f907e2e5402d104c93dcfa620ada09bf9
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-29 10:21:50 +0100

    url array

--- mvcframework/app/libraries/Core.php
+++ mvcframework/app/libraries/Core.php
@@ -1,1 +1,25 @@
-Core
+<?php
+    // Core App class
+    class Core 
+    {
+        protected $currentController = 'Pages';
+        protected $currentNethod = 'index';
+        protected $params = [];
+
+        public function __construct()
+        {
+            print_r($this->getUrl());
+        }
+
+        public function getUrl() {
+            if (isset($_GET['url'])) {
+                $url = rtrim($_GET['url'],'/'); // get rid off the last /
+                // allows you to filter variables as string/number
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                // breaking it into an array
+                $url = explode('/', $url);
+                return $url;
+            }
+        }
+
+    }
