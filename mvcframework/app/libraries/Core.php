9d18d02070e2b33fbf6ba0aec87b0e62610f6d20
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-29 10:41:21 +0100

    new controller

--- mvcframework/app/libraries/Core.php
+++ mvcframework/app/libraries/Core.php
@@ -8,7 +8,16 @@
 
         public function __construct()
         {
-            print_r($this->getUrl());
+            $url = $this->getUrl();
+            // look in 'controllers' for the first value, ucwords will capitalize the first letter
+            if (file_exists('../app/controllers'.ucwords($url[0]).'.php')){
+                // will set a new controller
+                $this->currentController = ucwords($url[0]);
+                unset($url[0]);
+            }
+            // require controller
+            require_once '../app/controllers/'.$this->currentController.'.php';
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl() {
