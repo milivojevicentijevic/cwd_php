7ce51bda0d1da5ed16e1ee83e602abcf1203cb9f
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-29 21:17:50 +0100

    validate username email

--- mvcframework/app/controllers/Users.php
+++ mvcframework/app/controllers/Users.php
@@ -16,6 +16,39 @@
                 'passwordError' => '',
                 'confirmPasswordError' => '' 
             ];
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // sanitize post data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $data = [
+                    'username' => trim($data['username']),
+                    'email' => trim($data['email']),
+                    'password' => trim($data['password']),
+                    'confirmPassword' => trim($data['confirmPassword']),
+                    'usernameError' => '',
+                    'emailError' => '',
+                    'emailError' => '',
+                    'passwordError' => '',
+                    'confirmPasswordError' => '' 
+                ];
+                $nameValidation = "/^[a-zA-Z0-9]*$/";
+                // validate username on letters/numbers
+                if (empty($data['username'])) {
+                    $data['usernameError'] = 'Please enter username.';
+                } elseif (!preg_match($nameValidation, $data['username'])) {
+                    $data['usernameError'] = 'Name can only contain letters and numbers.';
+                }
+                // validate email
+                if (empty($data['eamil'])) {
+                    $data['emailError'] = 'Please enter email address.';
+                } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                    $data['emailError'] = 'Please enter the correct format.';
+                } else {
+                    // check if email exists
+                    if ($this->userModel->findUserByEmail($data['email'])) {
+                        $data['emailError'] = 'Email is already taken.';
+                    }
+                }
+            }
             $this->view('users/register', $data);
         }
         public function login() {
@@ -24,6 +57,7 @@
                 'usernameError' => '',
                 'passwordError' => ''
             ];
+
             $this->view('users/login', $data);
         }
     }
--- mvcframework/app/models/User.php
+++ mvcframework/app/models/User.php
@@ -6,9 +6,17 @@
     public function __construct() {
         $this->db = new Database;
     }
-    //public function getUsers() {
-       // $this->db->query("SELECT * FROM users");
-       // $result = $this->db->resultSet();
-       // return $result;
-    //}
+    // find user by email. Email is passed in by Controller
+    public function findUsersByEmail($email) {
+        // prepared statement
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        // email param will be binded with the email variable
+        $this->db->bind(':email', $email);
+        // check if email is already registered
+        if($this->db->rowCount > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
