a7d5c216143c4e1b04c337959c47fba7dfdc1ebe
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-30 10:30:35 +0100

    create

--- mvcframework/app/controllers/Posts.php
+++ mvcframework/app/controllers/Posts.php
@@ -21,6 +21,32 @@ class Posts extends Controller
             'titleError' => '',
             'bodyError' => ''
         ];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'user_id' => $_SESSION['user_id'],
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'titleError' => '',
+                'bodyError' => ''
+            ];
+            if (empty($data['title'])) {
+                $data['titleError'] = 'The title of a post cannot be empty';
+            }
+            if (empty($data['body'])) {
+                $data['bodyError'] = 'The body of a post cannot be empty';
+            }
+            if (empty($data['titleError']) && empty($data['bodyError'])) {
+                if ($this->postModel->addPost($data)) {
+                    header('Location: '.URLROOT.'/posts');
+                } else {
+                    die("Something went wrong");
+                }
+            } else {
+                $this->view('posts/create', $data);
+            }
+        }
         $this->view('posts/create', $data);
     }
     public function update() {
--- mvcframework/app/models/Post.php
+++ mvcframework/app/models/Post.php
@@ -11,4 +11,16 @@ class Post
         $results = $this->db->resultSet();
         return $results;
     }
+    public function addPost($data) {
+        $this->db->query('INSERT INTO posts (user_id, title, body) VALUES (:user_id, :title, :body)');
+        $this->db->bind(':user_id', $data['user_id']); 
+        $this->db->bind(':title', $data['title']); 
+        $this->db->bind(':body', $data['body']); 
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
