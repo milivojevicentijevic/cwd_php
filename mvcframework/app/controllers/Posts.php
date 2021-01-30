6062fb630d3deb19b72a301ca94dbd2c00d6e0a0
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-01-30 16:07:43 +0100

    updated

--- mvcframework/app/controllers/Posts.php
+++ mvcframework/app/controllers/Posts.php
@@ -57,8 +57,46 @@ class Posts extends Controller
             header("Location: ".URLROOT."/posts");
         }
         $data = [
-            'post' => $post
+            'post' => $post,
+            'title' => '',
+            'body' => '',
+            'titleError' => '',
+            'bodyError' => ''
         ];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'id' => $id,
+                'post' => $post,
+                'user_id' => $_SESSION['user_id'],
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'titleError' => '',
+                'bodyError' => ''
+            ];
+            if (empty($data['title'])) {
+                $data['titleError'] = 'The title of a post cannot be empty';
+            }
+            if (empty($data['body'])) {
+                $data['bodyError'] = 'The body of a post cannot be empty';
+            }
+            if ($data['title'] == $this->postModel->findPostById($id)->title) {
+                $data['titleError'] = 'At least change the title!';
+            }
+            if ($data['body'] == $this->postModel->findPostById($id)->body) {
+                $data['bodyError'] = 'At least change the body!';
+            }
+            if (empty($data['titleError']) && empty($data['bodyError'])) {
+                if ($this->postModel->updatePost($data)) {
+                    header('Location: '.URLROOT.'/posts');
+                } else {
+                    die("Something went wrong");
+                }
+            } else {
+                $this->view('posts/update', $data);
+            }
+        }
  
         $this->view('posts/update', $data);
     }
